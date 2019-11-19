847412dca1d8deab8d21658c14eedae4aab8ec0a
Anthony Mattson <anthony.mattson@stelligent.com>
2019-11-19 15:18:08 -0500

    Added a rubocop ignore file.

--- spec/mini-project_spec.rb
+++ spec/mini-project_spec.rb
@@ -1,19 +1,17 @@
-require "./mini-project"
+require './mini-project'
 
 describe 'unittest_homepage' do
-    it 'displays the correct text and current time' do
-        time1 = 'Nov 14 13:50'
-        expect(homepage(time1)).to eq("Automation for the people!, it's #{time1} and its time to party!")
-    end
+  it 'displays the correct text and current time' do
+    time1 = 'Nov 14 13:50'
+    expect(homepage(time1)).to eq("Automation for the people!, it's #{time1} and its time to party!")
+  end
 end
 
-
-
 describe 'unittest_homepage_invalid' do
-    it 'displays the incorrect text and current time' do
-        time1 = 'Nov 14 13:50'
-        time2 = 'Nov 18 12:30'
-        valid_response = (homepage(time1)).to eq("Automation for the people!, it's #{time1} and its time to party!")
-        expect(homepage(time2)).not_to equal("Automation for the people!, it's #{time1} and its time to party!")
-    end
+  it 'displays the incorrect text and current time' do
+    time1 = 'Nov 14 13:50'
+    time2 = 'Nov 18 12:30'
+    homepage(time1).to eq("Automation for the people!, it's #{time1} and its time to party!")
+    expect(homepage(time2)).not_to equal("Automation for the people!, it's #{time1} and its time to party!")
+  end
 end
